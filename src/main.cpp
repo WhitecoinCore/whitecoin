01ff875e459e0a31449ef97a4ae558f67c5bcacd
Pavel Vasin <pavel@vasin.nl>
2015-04-14 19:44:59 +0300

    BIP16 is always active

--- src/main.cpp
+++ src/main.cpp
@@ -1860,9 +1860,8 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         }
     }
 
-    // BIP16 didn't become active until Apr 1 2012
-    int64_t nBIP16SwitchTime = 1333238400;
-    bool fStrictPayToScriptHash = (pindex->GetBlockTime() >= nBIP16SwitchTime);
+    // BIP16 is always active
+    bool fStrictPayToScriptHash = true;
 
     unsigned int flags = fStrictPayToScriptHash ? SCRIPT_VERIFY_P2SH : SCRIPT_VERIFY_NONE;
 
