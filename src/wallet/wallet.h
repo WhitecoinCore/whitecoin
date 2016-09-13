66f0068f3919b346a96e1a80d863ff0d20369b2d
Spencer Lievens <spencerlievens@users.noreply.github.com>
2016-09-13 23:30:32 +0100

    Addition of ImmatureCreditCached to MarkDirty()
    To protect against possible invalidation and to bring conformity to the code.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -342,6 +342,7 @@ public:
     {
         fCreditCached = false;
         fAvailableCreditCached = false;
+        fImmatureCreditCached = false;
         fWatchDebitCached = false;
         fWatchCreditCached = false;
         fAvailableWatchCreditCached = false;
